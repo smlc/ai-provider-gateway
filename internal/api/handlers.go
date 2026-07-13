270b7fc6b1d6b0929fdf36f655d1eb41706f4780
Marlon <7106775+smlc@users.noreply.github.com>
2026-07-13 22:11:53 +0100

    parse request

--- internal/api/handlers.go
+++ internal/api/handlers.go
@@ -1,19 +1,34 @@
 package api
 
-import "net/http"
-
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+	"sse-multiplexer/internal/models"
+)
 
 func HandleChatStream(w http.ResponseWriter, r *http.Request) {
+
+	
 	// Set headers for SSE
 	w.Header().Set("Content-Type", "text/event-stream")
 	w.Header().Set("Cache-Control", "no-cache")
 	w.Header().Set("Connection", "keep-alive")
-
-	w.Write([]byte("data: Here is a piece of data!\n\n"))
-	
-	if f, ok := w.(http.Flusher); ok {
-		f.Flush() // Flush the headers to the client
+	log.Println("Handler received request")
+	// Parse request body to ChatRequest type
+	var requestBody models.ChatRequest
+	if err := json.NewDecoder(r.Body).Decode(&requestBody); err != nil {
+		http.Error(w, "invalid request body", http.StatusBadRequest)
+		return
 	}
 	
+	log.Println("Request body ", requestBody)
+	w.Write([]byte("data: Here is a piece of data!\n\n"))
+
+	// if f, ok := w.(http.Flusher); ok {
+	// 	f.Flush() // Flush the headers to the client
+	// }
+
+	http.ResponseWriter.WriteHeader(w, http.StatusOK)
 
 }
